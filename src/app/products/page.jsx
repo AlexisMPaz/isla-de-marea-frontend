557a4f15811b8fcbaabfd8410425784f59f2ccca
Alexis Paz <alexismpaz@gmail.com>
2023-07-20 21:46:27 -0300

    Test

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -25,6 +25,7 @@ export default function ProductsPage() {
   const fetchProducts = async () => {
     try {
       console.log(process.env.NEXT_PUBLIC_API_URL)
+      console.log(process.env.API_URL)
       let url = `${process.env.NEXT_PUBLIC_API_URL}/api/products`;
       if (selectedCategory) {
         url += `?category=${selectedCategory}`;
